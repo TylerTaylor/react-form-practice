3924f641ddcf20d338d37b5a80195d1bbe81cec3
TylerTaylor <vikingtyty@gmail.com>
2023-02-14 10:55:20 -0600

    Add todo-3 solution

--- src/NewPet.js
+++ src/NewPet.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-const NewPet = () => {
+const NewPet = ({ setPets }) => {
 
   // TODO 1. Create state to hold your form data, whether individual states (name, species) or an object ({ name: '', species: '' })
   const [formData, setFormData] = useState({
@@ -21,11 +21,24 @@ const NewPet = () => {
   }
 
   // TODO 3. Wire up the form element with a function to handle submitting the form. This function will need to send a POST request, AND add your new object to the array of Pets (from App.js) - how can we get access to that setter function from here? How do we add to an array non-destructively?
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    fetch('http://localhost:8004/pets', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(formData)
+    })
+      .then(res => res.json())
+      .then(data => setPets(pets => [...pets, data]))
+  }
 
   return (
     <div className="form-container">
       <h1>Add Your Pet</h1>
-      <form className="pet-form">
+      <form className="pet-form" onSubmit={handleSubmit}>
         <input 
           type="text" 
           placeholder="Name"
