2c0f4c7a4b63751bc201c33b6246e7f1ee14fe75
TylerTaylor <vikingtyty@gmail.com>
2023-02-14 10:51:26 -0600

    Add todo-2 solution

--- src/NewPet.js
+++ src/NewPet.js
@@ -13,6 +13,12 @@ const NewPet = () => {
   // const [species, setSpecies] = useState('')
 
   // TODO 2. Wire up your input elements - when you type in the input, it needs to trigger a function to set your state. Remember to add a `value` property that lines up with your state.
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  }
 
   // TODO 3. Wire up the form element with a function to handle submitting the form. This function will need to send a POST request, AND add your new object to the array of Pets (from App.js) - how can we get access to that setter function from here? How do we add to an array non-destructively?
 
@@ -24,11 +30,15 @@ const NewPet = () => {
           type="text" 
           placeholder="Name"
           name="name"
+          onChange={handleChange}
+          value={formData.name}
         />
         <input 
           type="text" 
           placeholder="Species" 
           name="species"
+          onChange={handleChange}
+          value={formData.species}
         />
 
         <input type="submit" value="Add your pet" className="btn" />
